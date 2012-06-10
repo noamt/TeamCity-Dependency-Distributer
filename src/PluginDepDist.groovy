0bd4c54847735755bc4784c2ff61b377128ebc7b
noamt <noamt@jfrog.org>
2012-06-10 14:07:35 +0300

    Add dependency grapes

--- src/PluginDepDist.groovy
+++ src/PluginDepDist.groovy
@@ -1,3 +1,6 @@
+@Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.5.1')
+@Grab(group = 'org.apache.httpcomponents', module =  'httpcore', version = '4.0.1')
+
 import groovyx.net.http.RESTClient
 import org.apache.http.entity.FileEntity
 
@@ -13,7 +16,7 @@ def tcVersion = pom.properties.'teamcity.version'.text()
 
 println("Preparing dependencies for TC v${tcVersion}")
 def tcDeps = pom.dependencyManagement.dependencies.dependency.findAll {
-  it.groupId.text().contains('org.jetbrains.teamcity')
+    it.groupId.text().contains('org.jetbrains.teamcity')
 }
 def m2Dir = new File("${System.properties['user.home']}/.m2/repository")
 def tcInstallationDir = new File(scriptProps.tc.install.dir)
@@ -21,43 +24,44 @@ def tcInstallationDir = new File(scriptProps.tc.install.dir)
 def restClient = new RESTClient(scriptProps.repo.url)
 restClient.auth.basic scriptProps.deployer.username, scriptProps.deployer.password
 restClient.encoder.'application/zip' = this.&encodeZipFile
+
 def encodeZipFile(Object data) throws UnsupportedEncodingException {
-  def entity = new FileEntity((File) data, "application/zip");
-  entity.setContentType("application/zip");
-  return entity
+    def entity = new FileEntity((File) data, "application/zip");
+    entity.setContentType("application/zip");
+    return entity
 }
 
 tcDeps.each {
 
-  def pathBuilder = new StringBuilder()
-  def groupId = it.groupId.text()
-  def artifactId = it.artifactId.text()
+    def pathBuilder = new StringBuilder()
+    def groupId = it.groupId.text()
+    def artifactId = it.artifactId.text()
 
-  if ('org.jetbrains.teamcity.agent'.equals(groupId)) {
-    pathBuilder.append('buildAgent/')
-  } else if ('org.jetbrains.teamcity.webapp'.equals(groupId)) {
-    pathBuilder.append('webapps/ROOT/WEB-INF/')
-  }
-  pathBuilder.append('lib/').append(artifactId).append('.jar')
+    if ('org.jetbrains.teamcity.agent'.equals(groupId)) {
+        pathBuilder.append('buildAgent/')
+    } else if ('org.jetbrains.teamcity.webapp'.equals(groupId)) {
+        pathBuilder.append('webapps/ROOT/WEB-INF/')
+    }
+    pathBuilder.append('lib/').append(artifactId).append('.jar')
 
-  def dependencyJarFile = new File(tcInstallationDir, pathBuilder.toString())
-  if (!dependencyJarFile.exists()) {
-    throw new IllegalArgumentException("Unable to find TC dep: ${artifactId}");
-  }
-  def artifactPath = "${groupId.replace('.', '/')}/${artifactId}/${tcVersion}/${artifactId}-${tcVersion}.jar"
-  def dependencyTarget = new File(m2Dir, artifactPath)
-  dependencyTarget.getParentFile().mkdirs()
-  dependencyTarget.createNewFile()
-  def src = new FileInputStream(dependencyJarFile)
-  def dst = new FileOutputStream(dependencyTarget)
-  dst << src
+    def dependencyJarFile = new File(tcInstallationDir, pathBuilder.toString())
+    if (!dependencyJarFile.exists()) {
+        throw new IllegalArgumentException("Unable to find TC dep: ${artifactId}");
+    }
+    def artifactPath = "${groupId.replace('.', '/')}/${artifactId}/${tcVersion}/${artifactId}-${tcVersion}.jar"
+    def dependencyTarget = new File(m2Dir, artifactPath)
+    dependencyTarget.getParentFile().mkdirs()
+    dependencyTarget.createNewFile()
+    def src = new FileInputStream(dependencyJarFile)
+    def dst = new FileOutputStream(dependencyTarget)
+    dst << src
 
-  println("Copied TC dep to: ${dependencyTarget.getAbsolutePath()}")
+    println("Copied TC dep to: ${dependencyTarget.getAbsolutePath()}")
 
-  def response = restClient.put(path: artifactPath,
-          body: dependencyJarFile,
-          requestContentType: 'application/zip'
-  )
-  assert response.status == 201, response.data.text
-  println("Deployed TC dep to: ${restClient.getUri().toString() + artifactPath}")
+    def response = restClient.put(path: artifactPath,
+            body: dependencyJarFile,
+            requestContentType: 'application/zip'
+    )
+    assert response.status == 201, response.data.text
+    println("Deployed TC dep to: ${restClient.getUri().toString() + artifactPath}")
 }
